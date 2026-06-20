1e4faf74e213798cb46f57c8d36ce44493d79175
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-06-21 00:17:03 +0400

    Added listening function

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import speech_recognition
-import pyttsx3
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+def listenPrint():
+    with sr.Microphone() as source:
+        print("Listening....")
+
+        audio = recognizer.listen(source)
+        text = recognizer.recognize_google(audio)
+        print(f"You said: {text}")
+
+listenPrint()
