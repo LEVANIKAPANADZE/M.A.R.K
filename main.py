6bc45685c74e89d88a4fc168d3eca30a7ab257d0
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-06-29 23:33:14 +0400

    Deleted test file

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import speech_recognition as sr
 recognizer = sr.Recognizer()
 
 def listenPrint():
-    with sr.Microphone(device_index=2) as source:
+    with sr.Microphone() as source:
         print("Listening....")
+        recognizer.adjust_for_ambient_noise(source, duration=1)
 
         audio = recognizer.listen(source)
         text = recognizer.recognize_google(audio)
         print(f"You said: {text}")
 
-listenPrint()
+listenPrint()    
